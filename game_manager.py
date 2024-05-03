1a73001e677bf4fef86b25445847383b6e931f80
Maximilian Fernaldy <maximilian.fernaldy@gmail.com>
2024-05-03 19:28:41 +0900

    levels try 2

--- game_manager.py
+++ game_manager.py
@@ -3,33 +3,33 @@ import pygame
 from entities import Enemy, Player
 from ui_elements import HealthBar
 
-LEVEL_DATA = {
-    "1": {
-        "map": "./resources/cyberpunk-street-files/Version 1/PNG/cyberpunk-street.png",
-        "scale": 3.75,
-        "traversable_area": [0, 450, 2280, 270],
-        "player_health": 100,
-        "player_initial_position": [40, 400],
-        "enemy_positions": [
-            [40,450],
-            [800, 350],
-        ],
-        "enemy_damage": 5
-    },
-    "2": {
-        "map": "./resources/Miami-synth-files/Layers/highway.png",
-        "scale": 3,
-        "traversable_area": [0, 400, 2688, 320],
-        "player_health": 100,
-        "player_initial_position": [40, 400],
-        "enemy_positions": [
-            [400, 550],
-            [1200, 650],
-            [1000, 490]
-        ],
-        "enemy_damage": 10
-    }
-}
+# LEVEL_DATA = {
+#     "1": {
+#         "map": "./resources/cyberpunk-street-files/Version 1/PNG/cyberpunk-street.png",
+#         "scale": 3.75,
+#         "traversable_area": [0, 450, 2280, 270],
+#         "player_health": 100,
+#         "player_initial_position": [40, 400],
+#         "enemy_positions": [
+#             [40,450],
+#             [800, 350],
+#         ],
+#         "enemy_damage": 5
+#     },
+#     "2": {
+#         "map": "./resources/Miami-synth-files/Layers/highway.png",
+#         "scale": 3,
+#         "traversable_area": [0, 400, 2688, 320],
+#         "player_health": 100,
+#         "player_initial_position": [40, 400],
+#         "enemy_positions": [
+#             [400, 550],
+#             [1200, 650],
+#             [1000, 490]
+#         ],
+#         "enemy_damage": 10
+#     }
+# }
 
 class GameInstance:
     def __init__(self, screen_dimensions, window_title):
@@ -40,12 +40,12 @@ class GameInstance:
             self.level_data = json.load(level_file)
 
         self.game_state = "start screen"
-        self.current_stage = 0
+        self.current_stage = 1
         self.current_level = Level(self)
-    def start_next_stage(self):
-        self.current_stage += 1
+    def start_stage(self, stage):
+        self.current_stage = stage
         self.current_level = Level(self)
-        self.current_level.start_from_beginning()
+        self.game_state = "play"
 
 class Level:
     def __init__(self, game_instance):
@@ -54,8 +54,7 @@ class Level:
 
         current_stage_data = self.game_instance.level_data[str(stage)]
 
-        self.protags = pygame.sprite.Group()
-        self.enemies = pygame.sprite.Group()
+        self.enemies = []
         self.camera_group = CameraGroup()
 
         self.player_max_health = current_stage_data["player_health"]
@@ -66,15 +65,20 @@ class Level:
         self.map = Map(current_stage_data["map"], current_stage_data["scale"], pygame.Rect(*current_stage_data["traversable_area"]))
 
     def start_from_beginning(self):
+        print(f"Starting from the beginning of stage {self.game_instance.current_stage}")
         # Reset entities and sprite groups
-        self.protags.empty()
-        self.enemies.empty()
+        self.camera_group.empty()
+        self.enemies = []
         self.player = Player(self.player_max_health, self.player_initial_position)
         self.player.rect.topleft = self.player_initial_position
-        self.protags.add(self.player)
-        for enemy in self.enemies:
-            enemy.health = enemy.max_health
-            print(enemy.max_health)
+        self.camera_group.add(self.player)
+
+        print(self.enemies_data)
+        for enemy in self.enemies_data:
+            enemy_copy = enemy.copy()
+            self.enemies.append(enemy_copy)
+            self.camera_group.add(enemy_copy)
+        print(self.enemies)
         
         # Construct UI elements
         self.health_bar = HealthBar(self.player_max_health)
@@ -88,19 +92,23 @@ class Level:
     def update(self, screen):
         # Update all entities
         screen.fill("black")
-        self.enemies.update(self.player)
-        self.protags.update(self.enemies, self.map)
-
-        self.map.update()
-
-        self.camera_group.custom_draw(self.player, self.map)
+        for enemy in self.enemies:
+            enemy.update(self.player)
+        self.player.update(self.enemies, self.map)
 
         # Update UI elements
         self.health_bar.update(self.player.health, screen)
 
+        self.camera_group.custom_draw(self.player, self.map)
+
         # Check win condition
-        if [enemy for enemy in self.enemies if enemy.health > 0] == []:
+        alive_enemies = [enemy for enemy in self.enemies if enemy.health > 0]
+        if alive_enemies == []:
             self.game_instance.game_state = "won"
+        
+        # Check player health
+        if self.player.health <= 0:
+            self.game_instance.game_state = "game over"
 
         pygame.display.update()
 
@@ -111,8 +119,6 @@ class Map:
         self.background_rect = self.background.get_rect()
         self.background_rect.topleft = (0, 0)
         self.traversable_rect = traversable_rect
-    def update(self):
-        pass
 
 class CameraGroup(pygame.sprite.Group):
     def __init__(self):
@@ -137,7 +143,7 @@ class CameraGroup(pygame.sprite.Group):
 
         self.move_camera(player, map)
 
-        # Draw map
+        # Move map relative to box camera
         map_offset = map.background_rect.topleft - self.offset
 
         # Compensate for transparent space in entity PNG files
--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
     clock = pygame.time.Clock()
     frames_per_second = 60
 
-    start_menu_items = [Menu.MenuItemTextOnly("New Game", (640, 360), game_instance.start_next_stage)]
+    start_menu_items = [Menu.MenuItemTextOnly("New Game", (640, 360), game_instance.current_level.start_from_beginning)]
     start_menu = Menu(game_instance.screen, "./resources/UI/pause_menu.png", start_menu_items)
 
     continue_menu_items = [Menu.MenuItemTextOnly("Next level", (640, 360), game_instance.start_next_stage)]
@@ -109,8 +109,6 @@ def main():
             game_over_menu.update()
         elif game_instance.game_state == "play":
             game_instance.current_level.update(game_instance.screen)
-            if game_instance.current_level.player.health <= 0:
-                game_instance.game_state = "game over"
         elif game_instance.game_state == "pause":
             pause_menu.update()
         elif game_instance.game_state == "won":
